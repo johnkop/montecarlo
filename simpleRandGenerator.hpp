4d470928a8434f05617c96d058240de26006b1e1
johnkop <johnkop@hotmail.com>
2017-06-07 20:49:18 +0100

    /

--- simpleRandGenerator.hpp
+++ simpleRandGenerator.hpp
@@ -1,10 +1,3 @@
-//
-//  simpleRandGenerator.hpp
-//  c++
-//
-//  Created by JK on 06/06/2017.
-//  Copyright © 2017 JK. All rights reserved.
-//
 
 #ifndef simpleRandGenerator_hpp
 #define simpleRandGenerator_hpp
