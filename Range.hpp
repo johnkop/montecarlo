70eba41833a5acce2156ad315b5e9ebb76f1a8dd
johnkop <johnkop@hotmail.com>
2017-06-16 21:58:45 +0100

    implements range

--- Range.hpp
+++ Range.hpp
@@ -13,23 +13,23 @@
 
 template <class T> class Range {
 private:
-  T lo;
-  T hi;
+  T lo; // represents the begin of a mesh
+  T hi; // represents the end of a mesh
   
 public:
   Range();
-  Range(const T& low, const T& high);
-  Range(const Range<T>& ran2);
+  Range(const T& low, const T& high); //constructor
+  Range(const Range<T>& ran2); // constructor by copy
   
   virtual ~Range();//destructor
   
-  void low(const T& t);
-  void high(const T& t);
+  void low(const T& t); // sets the lo
+  void high(const T& t); // sets the hi
   
-  T low() const;
-  T high() const;
+  T low() const; // get lo
+  T high() const; // get hi
   
-  T spread() const;
+  T spread() const; // get hi-lo
   
   bool left(const T& value) const;
   bool right(const T& value) const;
@@ -41,5 +41,40 @@ public:
   
   
 };
+
+
+template <class T> Range<T>::Range(const T& low, const T& high){
+  lo = low;
+  hi = high;
+}
+
+//need to do a deep copy code work
+template <class T> Range<T>::Range(const Range<T>& ran2){
+  lo = ran2.lo;
+  hi = ran2.hi;
+}
+
+template <class T> Range<T>::low(const T& t){
+  lo = t;
+  return;
+}
+template <class T> Range<T>::high(const T& t){
+  hi = t;
+  return;
+}
+template <class T> Range<T>::low(){
+  return lo;
+}
+template <class T> Range<T>::hi(){
+  return hi;
+}
+template <class T> Range<T>::spread(){
+  return hi-lo;
+}
+
+bool Range<T>::contains(const T& value){
+  return (value>=lo) && (value<=hi);
+}
+
 
 #endif /* Range_hpp */
